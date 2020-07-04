08ca6bdeaa11a2434fd58b70c9d154432027d007
Josehower <josehowerupn@gmail.com>
2020-07-04 21:38:37 +0200

    switch challenge complete

--- switch/app/script.js
+++ switch/app/script.js
@@ -1,17 +1,27 @@
 const switchContainer = document.querySelector('.switch_container');
+const moon = document.querySelector('.moon');
+const sun = document.querySelector('.sun');
 
-let switchState = false;
+let switchState = true;
 
 function toggleSwitch(actualState) {
-  switchState = !actualState;
+  const icon = switchState ? [sun, moon] : [moon, sun];
+  icon[0].classList.add('hide');
+  icon[1].classList.remove('hide');
+
   switchContainer.style.justifyContent = switchState
     ? 'flex-start'
     : 'flex-end';
   switchContainer.style.backgroundColor = switchState
-    ? 'var(--color-day-bg)'
-    : 'var( --color-night-bg)';
+    ? 'var(--color-night-bg)'
+    : 'var( --color-day-bg)';
+
+  switchState = !actualState;
 }
 
 switchContainer.addEventListener('click', () => {
   toggleSwitch(switchState);
+});
+switchContainer.addEventListener('keyup', e => {
+  if (e.key === 'Enter') toggleSwitch(switchState);
 });
